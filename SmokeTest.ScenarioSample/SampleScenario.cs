d62f16f5b76303ae98579bc474b5e45e0d0e6c29
Jean-Michel <jmderuty@stormancer.com>
2017-05-15 18:22:38 +0200

    Added sample smoketest scenario.

---
+++ SmokeTest.ScenarioSample/SampleScenario.cs
@@ -0,0 +1,27 @@
+﻿using Stormancer.Monitoring.SmokeTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmokeTest.ScenarioSample
+{
+    public class SampleScenario : IScenario
+    {
+        public string Name
+        {
+            get
+            {
+                return "sample";
+            }
+        }
+
+        public async Task<Dictionary<string, float>> Run(dynamic configuration, string[] args)
+        {
+            await Task.FromResult(true);
+
+            return new Dictionary<string, float> { { "test", new Random().Next(100) } };
+        }
+    }
+}
